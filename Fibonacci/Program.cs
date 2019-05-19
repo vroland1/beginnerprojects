64163de7b1c3ee9464b909b3ca00ffe52aaebea1
Tory Roland <50778995+vroland1@users.noreply.github.com>
2019-05-19 00:11:21 -0400

    Add files via upload
    Quick program I created to list the Fibonacci sequence leading up to a number input from the user!

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fibNumberCurrent = 0;
+            int fibNumberPrevious = 0;
+            int fibNumberNext = 0;
+
+            Console.WriteLine("Please enter a positive integer");
+            string numberInput = Console.ReadLine();
+            int numberInputAsAnInteger = int.Parse(numberInput);
+            Console.WriteLine();
+
+            while (fibNumberCurrent <= numberInputAsAnInteger)
+            {
+                if (fibNumberCurrent == 0)
+                {
+                    Console.Write("The Fibonacci number sequence leading up to your number is: " + fibNumberCurrent);
+                    fibNumberPrevious = fibNumberCurrent;
+                    fibNumberCurrent = fibNumberCurrent + 1;
+                   
+                }
+                else 
+                {
+                    Console.Write(", " + fibNumberCurrent);
+                    fibNumberNext = fibNumberCurrent + fibNumberPrevious;
+                    fibNumberPrevious = fibNumberCurrent;
+                    fibNumberCurrent = fibNumberNext;
+                }
+
+            }
+            Console.ReadLine();
+
+            
+
+           
+        }
+    }
+}
